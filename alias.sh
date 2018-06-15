a9a378988dac876e543833201338c58ee86787a8
Luke Libraro <luke@rw.co>
2018-06-15 13:50:41 -0500

    modified to not be in home directory

--- alias.sh
+++ alias.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 setToken() {
     #do things with parameters like $1 such as
-    ~/mfa.sh $1 $2
-    source ~/.token_file
+    ~/.aws-mfa/mfa.sh $1 $2
+    source ~/.aws-mfa/.token_file
     echo "Your creds have been set in your env."
 }
 alias mfa=setToken
